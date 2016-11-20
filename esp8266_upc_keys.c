c2f313884504834a606303c5db6ba1335f09cfe5
Aram <aram@factorit.nl>
2016-11-20 07:59:16 +0100

    Added a bit of speed optimization by inlining

--- esp8266_upc_keys.c
+++ esp8266_upc_keys.c
@@ -513,7 +513,7 @@ uint32_t mangle(uint32_t *pp)
 
 __attribute((optimize("O3")))
 ICACHE_FLASH_ATTR
-static
+inline
 uint32_t upc_generate_ssid(uint32_t* data, uint32_t magic)
 {
     uint64_t a = data[0] * 2500000 + data[1] * 6800 + data[2] + magic;
